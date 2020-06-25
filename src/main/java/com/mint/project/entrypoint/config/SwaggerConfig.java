8583fc61a0d07baf0d8afc4387d7d27315825798
Bruno <onyedika.okafor@jumia.com>
2020-06-25 18:12:04 +0100

    Added Swagger

---
+++ src/main/java/com/mint/project/entrypoint/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.mint.project.entrypoint.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Bruno Okafor 2020-06-25
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.mint.project"))
+				.build();
+	}
+}
