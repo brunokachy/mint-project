a67ea114f0958a322bb880a104a41e23881ad164
Bruno <onyedika.okafor@jumia.com>
2020-06-25 12:51:55 +0100

    Initial Project Setup

---
+++ src/main/java/com/mint/project/MintProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mint.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MintProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mint/project/MintProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mint.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
